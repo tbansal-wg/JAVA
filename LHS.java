bcd2ed4fbbc2a31008aa2dedcf320a7cd623185f
TANVI BANSAL <126918761+TanviBansal29@users.noreply.github.com>
2023-06-14 19:52:20 +0530

    Add files via upload

---
+++ LHS.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LHS{
+    public static void main(String args[]){
+        HashSet<String> cities = new HashSet<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Noida");
+        cities.add("Bengaluru");
+
+
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("Mumbai");
+        lhs.add("Noida");
+        lhs.add("Bengaluru");
+
+        lhs.remove("Delhi");
+
+        System.out.println(lhs);  // Linkedhashset is ordered
+
+    
+    }
+}
