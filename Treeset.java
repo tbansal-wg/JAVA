09481aef81d90699eec48cd3bf74cf398dc36c7f
TANVI BANSAL <126918761+TanviBansal29@users.noreply.github.com>
2023-07-05 23:32:16 +0530

    Add files via upload

---
+++ Treeset.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Treeset{
+    public static void main(String[] args) {
+
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Delhi");
+        ts.add("Mumbai");
+        ts.add("Noida");
+        ts.add("Bengaluru");
+
+        System.out.println(ts);
+
+        // remove , size , clear , isEmpty, contains, iterator
+
+    }
+
+}
